7819d29a7e6a023c5040c372ad71a7e1c4bacf5d
Raajeev Chandran <raajeevchandran787@gmail.com>
2020-12-24 10:39:21 +0530

    fixing build crashes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Routes from './Routes'
 import ReactDOM from "react-dom"
-import {firebaseConfig} from "./firebaseConfig"
-import firebase from "firebase/app"
-firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(<Routes/>,document.getElementById('root'))
